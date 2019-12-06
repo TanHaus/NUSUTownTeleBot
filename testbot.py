25f649f57785dbac57ced331406a9c71d857f7ea
picasdan9 <58513950+picasdan9@users.noreply.github.com>
2019-12-06 12:18:50 +0700

    testbot from workshop

---
+++ testbot.py
@@ -0,0 +1,31 @@
+from TextToOwO import owo
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start(update, context):
+    update.message.reply_text("UwU")
+
+def help(update, context):
+    update.message.reply_text("Hewp! :o")
+
+def handle_text(update, context):
+    update.message.reply_text(owo.text_to_owo(update.message.text))
+
+
+def main():
+    updater = Updater("640769917:AAFoD9m-vyOaQ6sTbyW7ah-RGJW1XPD6c6A", use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    #dp.add_handler(CommandHandler("get_joke", getjoke))
+
+    dp.add_handler(MessageHandler(Filters.text, handle_text))
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__=='__main__':
+    main()
